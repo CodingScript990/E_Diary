bd80c54ab16db7bf9c8c07e4511dd9c9068b2dae
Minwookim <trte135@naver.com>
2022-04-21 23:58:36 +0900

    [E_Diary]  - fontawesome 라이브러리 사용  - Emoticon 사용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import "./App.css";
 // react-router-dom@6
 import { BrowserRouter as Container, Routes, Route } from "react-router-dom";
+// url
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
-import RouterTest from "./components/RouterTest";
 
 function App() {
   return (
     <Container>
       <div className="App">
         <h2>hi?</h2>
+
+        <i className="fa-solid fa-face-kiss-wink-heart" />
+        <i className="fa-solid fa-face-grin-wide" />
+        <i className="fa-solid fa-face-meh-blank" />
+        <i className="fa-solid fa-face-rolling-eyes" />
+        <i className="fa-solid fa-face-frown" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/diary/:id" element={<Diary />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/new" element={<New />} />
         </Routes>
-        <RouterTest />
       </div>
     </Container>
   );
